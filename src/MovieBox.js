29449f1079e454a42422706da86a68bb6cd87099
Katie <e.v.rassolenko@gmail.com>
2023-06-28 21:14:46 +0300

    movie pages created

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 
 const imageURL = "https://image.tmdb.org/t/p/w500/"
 
-function MovieBox({ original_title, poster_path }) {
+function MovieBox({ original_title, poster_path, vote_average, release_date, overview }) {
+
+    const [show, setShow] = useState(false);
+
+    const handleShow = () => {setShow(true); console.log(show)};
+    const handleClose = () => {setShow(false); console.log(show)};
+
+
     return (
-        <div className="card" style={{ width: "16rem" }}>
-            <img classname="card-img-top hover:scale-105 ease-in-out duration-300" src={imageURL + poster_path} alt="Card image cap" />
+        <div className="card" onClick={handleShow} style={{ width: "16rem" }}>
+            <img classname="card-img-top" src={imageURL + poster_path} alt="Card image cap" />
             <div className="card-body">
                 <h5 className="card-text">{original_title}</h5>
+                <Modal show={show}>
+                    <Modal.Header>
+                        <Modal.Title>
+                        <h3 className="card-text">{original_title}</h3>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <img classname="card-img-top " src={imageURL + poster_path} style={{ width: "29rem" }} />
+                        <h5 className="card-text"> ImDb: {vote_average}</h5>
+                        <h5 className="card-text"> Release date: {release_date}</h5>
+                        <h5 className="card-text"> Overview:</h5>
+                        <p className="card-text">  {overview}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button className="btn btn-outline-secondary my-2 my-sm-0" onClick={handleClose}>CLOSE</button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         </div>
     )
--- src/tvBox.js
+++ src/tvBox.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+
 
 const imageURL = "https://image.tmdb.org/t/p/w500/"
 
-function TvBox({ name, poster_path }) {
+function TvBox({ name, poster_path, vote_average, first_air_date }) {
+
+    const [show, setShow] = useState(false);
+
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
     return (
-        <div className="card" style={{ width: "16rem" }}>
+        <div className="card" onClick={handleShow} style={{ width: "16rem" }} >
             <img classname="card-img-top" src={imageURL + poster_path} alt="Card image cap" />
             <div className="card-body">
                 <h5 className="card-text">{name}</h5>
+                <Modal show={show}>
+                    <Modal.Header>
+                        <Modal.Title>
+                        <h3 className="card-text">{name}</h3>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <img classname="card-img-top " src={imageURL + poster_path} style={{width: "29rem"}}/>
+                        <h5 className="card-text"> ImDb: {vote_average}</h5>
+                        <h5 className="card-text"> Release date: {first_air_date}</h5>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button className="btn btn-outline-secondary my-2 my-sm-0" onClick={handleClose}>CLOSE</button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         </div>
     )
