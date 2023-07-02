fcb53fbb0f6504d02f086e3f17b9b827065283be
Katie <e.v.rassolenko@gmail.com>
2023-07-02 23:03:54 +0300

    modal is working and dropdown menu got some css style

--- src/Filter.js
+++ src/Filter.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function Filter(props) {
     return (
-        <div class="dropdown">
-            <select value={props.filter} onChange={props.filterMovie}>
+        <div >
+            <select class="theDiv" value={props.filter} onChange={props.filterMovie}>
+                <option value="" selected disabled hidden>Choose ganre</option>
                 <option value={18}>Drama</option>
                 <option value={35}>Comedy</option>
                 <option value={16}>Animation</option>
--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Modal } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const imageURL = "https://image.tmdb.org/t/p/w500/"
 
@@ -7,8 +8,8 @@ function MovieBox({ original_title, poster_path, vote_average, release_date, ove
 
     const [show, setShow] = useState(false);
 
-    const handleShow = () => {setShow(true); console.log(show)};
-    const handleClose = () => {setShow(false); console.log(show)};
+    const handleClose = () => setShow(!show);
+    const handleShow = () => setShow(!show);
 
 
     return (
@@ -16,8 +17,8 @@ function MovieBox({ original_title, poster_path, vote_average, release_date, ove
             <img classname="card-img-top" src={imageURL + poster_path} alt="Card image cap" />
             <div className="card-body">
                 <h5 className="card-text">{original_title}</h5>
-                <Modal show={show}>
-                    <Modal.Header>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
                         <Modal.Title>
                         <h3 className="card-text">{original_title}</h3>
                         </Modal.Title>
@@ -30,7 +31,7 @@ function MovieBox({ original_title, poster_path, vote_average, release_date, ove
                         <p className="card-text">  {overview}</p>
                     </Modal.Body>
                     <Modal.Footer>
-                        <button className="btn btn-outline-secondary my-2 my-sm-0" onClick={handleClose}>CLOSE</button>
+                        <Button className="secondary" onClick={handleClose}>CLOSE</Button>
                     </Modal.Footer>
                 </Modal>
             </div>
